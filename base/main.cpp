9e5efe5bc6208378553ed8f8f788a8d000a309df
chopperdodo <zhangq.hit@gmail.com>
2015-01-17 17:29:23 -0800

    Add command line to input problem number

--- base/main.cpp
+++ base/main.cpp
@@ -13,8 +13,26 @@
 
 int main(int argc, const char * argv[]) {
     
-    BaseSolution *p_solution = nullptr;
-    int problem_num = 174;
+    BaseSolution *p_solution = NULL;
+    int problem_num = -1;
+    
+    for (int i = 0; i < argc; ++i) {
+        const char *op = argv[i];
+        
+        if (strcmp(op, "-h") == 0) {
+            printf("Call Help Function\n");
+            exit(0);
+        } else
+            if (strcmp(op, "-n") == 0) {
+                problem_num = atoi(argv[++i]);
+                printf("Choose Problem <%d>\n", problem_num);
+            }
+    }
+    
+    if (problem_num < 0) {
+        printf("Invalid Problem number=%d\n", problem_num);
+        exit(-1);
+    }
     
     p_solution = getSolutionClass(problem_num);
     
